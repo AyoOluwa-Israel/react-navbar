7bfb1868c35d24749f85ef390a67769b7bed9c22
AyoOluwa-Israel <izzyfresher@gmail.com>
2020-10-09 14:57:27 +0100

    initail

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -7,21 +7,27 @@ import Burger from './Burger';
 const Nav = styled.nav `
   width: 100%;
   height: 55px;
-  border-bottom:20px solid #f1f1f1;
+  box-shadow: 1px 0px 5px 0px;
   padding: 0 20px;
   display: flex;
   justify-content: space-between;
 
   .logo{
-    padding: 15px 0;
+    padding-left: 4em; 
   }
+  img{
+    width: auto;
+    height: 50px;
+  }
+
+  @media
 
 `;
 const Navbar = () => {
   return (
     <Nav>
       <div className="logo">
-        Navbar
+        <img src="img/logo192.png" alt=""/>
       </div>
       <Burger/>
     </Nav>
