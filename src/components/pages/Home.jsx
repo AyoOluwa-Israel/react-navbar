94cfe08a8de1fae547a43047effe61fb131f24d4
AyoOluwa-Israel <izzyfresher@gmail.com>
2020-10-16 18:57:37 +0100

    2nd Commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import Cards from '../Cards'
-import HeroSection from '../HeroSection'
-import JobsApi from '../../JobsApi'
+import React from 'react';
+import HeroSection from '../HeroSection';
+import JobsApi from '../../JobsApi';
 
 const Home = () => {
   return (
